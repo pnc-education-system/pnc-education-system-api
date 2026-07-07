05b145d20aa4d1ddc6db80376581e570578f2829
NimSokny <sokny.nim@student.passerellesnumeriques.org>
2026-07-07 11:08:29 +0700

    add folder v1

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\Api\V1\RoleController;
 
 /*
 |--------------------------------------------------------------------------
