f1d6046099007b5a1f5f2dbaeb8047442790c0e0
soengvicheka <soengvicheka775@gmail.com>
2026-07-10 10:19:33 +0700

    feat: commit change

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,18 @@
 <?php
+
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\RoleController;
+use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\AuthController;
-use App\Http\Controllers\Api\V1\UserController;
-use App\Http\Controllers\Api\V1\RoleController;
+
 Route::prefix('v1')->group(function () {
     Route::prefix('auth')->group(function () {
         Route::post('login', [AuthController::class, 'login'])->middleware('throttle:5,1');
         Route::post('password/reset', [AuthController::class, 'requestReset'])->middleware('throttle:3,1');
         Route::post('password/reset/confirm', [AuthController::class, 'confirmReset'])->middleware('throttle:3,1');
     });
+
     Route::middleware('jwt.auth')->group(function () {
         Route::post('auth/logout', [AuthController::class, 'logout']);
         Route::post('auth/refresh', [AuthController::class, 'refresh']);
@@ -19,6 +22,7 @@ Route::prefix('v1')->group(function () {
             Route::apiResource('users', UserController::class);
             Route::patch('users/{user}/toggle', [UserController::class, 'toggle']);
         });
+
         Route::middleware('permission:roles.manage')->group(function () {
             Route::get('roles', [RoleController::class, 'index']);
             Route::get('roles/{id}', [RoleController::class, 'show']);
